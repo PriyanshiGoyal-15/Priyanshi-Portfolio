838cf8578e5d0617c1e536e117fc6753681c11f6
Priyanshi Goyal <priyanshi3625@gmail.com>
2025-11-27 19:07:22 +0530

    commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    
+
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
@@ -13,4 +13,12 @@ export default defineConfig({
       },
     },
   },
+
+  optimizeDeps: {
+    include: [
+      "bootstrap/dist/css/bootstrap.min.css",
+      "bootstrap/dist/js/bootstrap.bundle.min.js",
+      "bootstrap-icons/font/bootstrap-icons.css"
+    ]
+  }
 });
